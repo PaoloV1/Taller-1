4b711b819b3a60cc10209b56017540c75ea3dab9
PaoloV1 <paolo.vera.rivera@gmail.com>
2023-04-06 02:54:49 -0400

    Lineas mas ordenadas y colision solo en la primera linea

--- src/Main.java
+++ src/Main.java
@@ -22,9 +22,9 @@ public class Main {
 
         /* Defino las variables de posicion
         * en este caso defino la posicion de la primera linea al azar
-        * y las otras 5 lineas las genero en base a la posicion de la primera linea
-        * y utilizo un numero al azar dividido por 10 para poder separarlas una distancia logica
-        * para que estas puedan ser apreciadas y distingidas una de la otra
+        * y las otras 5 lineas las genero en base a la posicion de la linea
+        * generada anteriormente restandola por un numero generado al azar divido por 10
+        * para que la distancia sea lo necesario para que se distingan pero para que no esten tan separadas
         * */
 
         //Posicion Primera linea
@@ -37,43 +37,43 @@ public class Main {
 
         //Posicion Segunda Linea
 
-        double x2 = x0 + (Math.random() / 10);
-        double y2 = y0 + (Math.random() / 10);
+        double x2 = x0 - (Math.random() / 10);
+        double y2 = y0 - (Math.random() / 10);
 
-        double x3 = x1 + (Math.random() / 10);
-        double y3 = y1 + (Math.random() / 10);
+        double x3 = x1 - (Math.random() / 10);
+        double y3 = y1 - (Math.random() / 10);
 
         //Posicion Tercera linea
 
-        double x4 = x0 + (Math.random() / 10);
-        double y4 = y0 + (Math.random() / 10);
+        double x4 = x2 - (Math.random() / 10);
+        double y4 = y2 - (Math.random() / 10);
 
-        double x5 = x1 + (Math.random() / 10);
-        double y5 = y1 + (Math.random() / 10);
+        double x5 = x3 - (Math.random() / 10);
+        double y5 = y3 - (Math.random() / 10);
 
         //Posicion Cuarta linea
 
-        double x6 = x0 + (Math.random() / 10);
-        double y6 = y0 + (Math.random() / 10);
+        double x6 = x4 - (Math.random() / 10);
+        double y6 = y4 - (Math.random() / 10);
 
-        double x7 = x1 + (Math.random() / 10);
-        double y7 = y1 + (Math.random() / 10);
+        double x7 = x5 - (Math.random() / 10);
+        double y7 = y5 - (Math.random() / 10);
 
         //Posicion Quinta linea
 
-        double x8 = x0 + (Math.random() / 10);
-        double y8 = y0 + (Math.random() / 10);
+        double x8 = x6 - (Math.random() / 10);
+        double y8 = y6 - (Math.random() / 10);
 
-        double x9 = x1 + (Math.random() / 10);
-        double y9 = y1 + (Math.random() / 10);
+        double x9 = x7 - (Math.random() / 10);
+        double y9 = y7 - (Math.random() / 10);
 
         //Posicion Sexta linea
 
-        double x10 = x0 + (Math.random() / 10);
-        double y10 = y0 + (Math.random() / 10);
+        double x10 = x8 - (Math.random() / 10);
+        double y10 = y8 - (Math.random() / 10);
 
-        double x11 = x1 + (Math.random() / 10);
-        double y11 = y1 + (Math.random() / 10);
+        double x11 = x9 - (Math.random() / 10);
+        double y11 = y9 - (Math.random() / 10);
 
 
         /* Defino la velocidad de las lineas
@@ -94,16 +94,8 @@ public class Main {
         while (true) {
 
 
-            /*
-            * Colision
-            *
-            * Quisiera hacer una observacion y es que intente simplificar el codigo para que no quedara tan largo
-            * con ciclos for pero, estos ciclos for no lograban a lo que queria llegar y a lo que se pide en el taller
-            * por lo que decidi no acortarlo pero ordenarlo cosa de que el codigo todavia sea entendible
-            * */
+            // Colision
 
-
-            //Colision Primera linea
 
             if (Math.abs(x0 + vx0) > 1.0) {
                 vx0 = -vx0;
@@ -119,85 +111,7 @@ public class Main {
                 vy = -vy;
             }
 
-            //Colision Segunda linea
 
-            if (Math.abs(x2 + vx0) > 1.0) {
-                vx0 = -vx0;
-            }
-            if (Math.abs(x3 + vx) > 1.0) {
-                vx = -vx;
-            }
-
-            if (Math.abs(y2 + vy0) > 1.0) {
-                vy0 = -vy0;
-            }
-            if (Math.abs(y3 + vy) > 1.0) {
-                vy = -vy;
-            }
-
-            //Colision Tercera linea
-
-            if (Math.abs(x4 + vx0) > 1.0) {
-                vx0 = -vx0;
-            }
-            if (Math.abs(x5 + vx) > 1.0) {
-                vx = -vx;
-            }
-
-            if (Math.abs(y4 + vy0) > 1.0) {
-                vy0 = -vy0;
-            }
-            if (Math.abs(y5 + vy) > 1.0) {
-                vy = -vy;
-            }
-
-            //Colision Cuarta linea
-
-            if (Math.abs(x6 + vx0) > 1.0) {
-                vx0 = -vx0;
-            }
-            if (Math.abs(x7 + vx) > 1.0) {
-                vx = -vx;
-            }
-
-            if (Math.abs(y6 + vy0) > 1.0) {
-                vy0 = -vy0;
-            }
-            if (Math.abs(y7 + vy) > 1.0) {
-                vy = -vy;
-            }
-
-            //Colision Quinta linea
-
-            if (Math.abs(x8 + vx0) > 1.0) {
-                vx0 = -vx0;
-            }
-            if (Math.abs(x9 + vx) > 1.0) {
-                vx = -vx;
-            }
-
-            if (Math.abs(y8 + vy0) > 1.0) {
-                vy0 = -vy0;
-            }
-            if (Math.abs(y9 + vy) > 1.0) {
-                vy = -vy;
-            }
-
-            //Colision Sexta linea
-
-            if (Math.abs(x10 + vx0) > 1.0) {
-                vx0 = -vx0;
-            }
-            if (Math.abs(x11 + vx) > 1.0) {
-                vx = -vx;
-            }
-
-            if (Math.abs(y10 + vy0) > 1.0) {
-                vy0 = -vy0;
-            }
-            if (Math.abs(y11 + vy) > 1.0) {
-                vy = -vy;
-            }
 
             // Movimiento de las lineas
 
